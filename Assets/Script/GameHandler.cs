f943ba90a3b7141c19d2c4fd5a1a4ceaf21480b0
bhusanSahoo <bhusan12sahoo@gmail.com>
2024-12-21 18:21:00 +0530

    .

--- Assets/Script/GameHandler.cs
+++ Assets/Script/GameHandler.cs
@@ -7,7 +7,7 @@ public class GameHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("I am game Handler.");
     }
 
     // Update is called once per frame
