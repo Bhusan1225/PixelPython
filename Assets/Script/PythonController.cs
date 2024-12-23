06e023f00b4cf1663d874c317b058a4e1705f948
bhusanSahoo <bhusan12sahoo@gmail.com>
2024-12-23 08:49:16 +0530

    .

--- Assets/Script/PythonController.cs
+++ Assets/Script/PythonController.cs
@@ -7,7 +7,7 @@ public class PythonController : MonoBehaviour
 
     public float pythonSpeed = 1f;
     Vector3 currentDirection = Vector3.up;
-    public foodSpawnManager spawnManager;
+    public AppleSpawnManager spawnManager;
 
     // Start is called before the first frame update
     void Start()
